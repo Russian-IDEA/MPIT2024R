5def226b5456481389f3dd0847d5af1de0cb43a6
LiptonItTea <stormtrooper.145145@gmail.com>
2024-11-28 14:46:25 +0300

    Fixed amount of attributes

--- MPITR/parsing.py
+++ MPITR/parsing.py
@@ -4,23 +4,30 @@ from lxml import etree
 parser = etree.XMLParser(encoding='utf-8',
                          recover=True)
 
+def parse_tags(root) -> list[str]:
+    tags = list()
+    added = set('param')
+    for element in root:
+        if element.tag not in added:
+            tags.append(element.tag)
+            added.add(element.tag)
 
-def parse_xml(root) -> dict:
+    return tags
+
+def parse_params_amount(offer) -> int:
+    return len(offer.findall('param'))
+
+
+def parse_xml(root, tags: list[str], params_amount: int) -> dict:
     offers = {}
     for offer in root.findall(".//offer"):
-        offerDict = {
-            "available": offer.attrib["available"],
-            "price": offer.find("price").text,
-            "currencyID": offer.find("currencyId").text,
-            "categoryId": offer.find("categoryId").text,
-            "picture": offer.find("picture").text,
-            "name": offer.find("name").text,
-            "vendor": offer.find("vendor").text,
-            "description": offer.find("description").text,
-            "barcode": offer.find("barcode").text,
-        }
-        for param in offer.findall("param"):
-            offerDict[param.attrib["name"]] = param.text
+        offerDict = list()
+        for tag in tags:
+            offerDict.append(offer.find(tag).text)
+
+        # params = offer.findall("param")
+        # for param in offer.findall("param"):
+        #     offerDict.append(param.text)
 
         offers[offer.attrib["id"]] = offerDict
     return offers
@@ -30,7 +37,9 @@ def parse_file(file_name: str = "../feeds/yandex_feed.xml") -> dict:
     tree = lxml.etree.parse(file_name, parser)
     root = tree.getroot()
 
-    offers = parse_xml(root)
+    tags = parse_tags(root.find('.//offer'))
+    params_amount = parse_params_amount(root.find('.//offer'))
+    offers = parse_xml(root, tags, params_amount)
 
     return offers
 
