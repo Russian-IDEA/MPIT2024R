259506b86e40877aba6068066f24a94138bc13f5
Andrey <andreydem42@gmail.com>
2024-11-28 14:10:10 +0300

    show table

--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
-import csv
-
+from MPITR.parsing import parse_file
 from django.shortcuts import render
 
 def home(request):
-    dictionary =
-    return render(request, 'index.html', {'table': table})
+    dictionary = parse_file('feeds/yandex_feed.xml')
+    columns = list(dictionary[list(dictionary.keys())[0]].keys())
+    return render(request, 'index.html',
+                  {'columns': columns, 'table': dictionary})
